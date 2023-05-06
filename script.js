749e0c98865d29d5981af946905c9fc2adaa7400
Raymond Zhong <rzhong669@gmail.com>
2023-05-06 15:30:16 -0500

    Add functionality for book checkboxes using IDs

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const overlayElement = document.querySelector(".overlay");
 const titleError = document.querySelector("#title + span.error");
 const authorError = document.querySelector("#author + span.error");
 const pagesError = document.querySelector("#pages + span.error");
+const containerDiv = document.querySelector(".container");
 
 addBooktoLibrary("a", "b", 12, true);
 listLibraryBooks();
@@ -38,14 +39,15 @@ function addBooktoLibrary(title, author, pages, isRead) {
 function listLibraryBooks() {
   // If library element exists, remove it
   if (document.querySelector(".library") !== null) {
+    // updateBookValues();
     document.querySelector(".library").remove();
   }
   const libraryDiv = document.createElement("div");
   libraryDiv.classList.add("library");
-  const containerDiv = document.querySelector(".container");
   containerDiv.appendChild(libraryDiv);
 
-  myLibrary.forEach((book) => {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book-container");
     libraryDiv.appendChild(bookDiv);
@@ -65,12 +67,12 @@ function listLibraryBooks() {
     const bookIsRead = document.createElement("input");
     bookIsRead.type = "checkbox";
     bookIsRead.checked = book.isRead;
-    bookIsRead.id = "isread";
+    bookIsRead.id = "book-" + i;
     bookDiv.appendChild(bookIsRead);
     const isReadLabel = document.createElement("label");
-    isReadLabel.htmlFor = "isread";
+    isReadLabel.htmlFor = "book-" + i;
     bookDiv.appendChild(isReadLabel);
-  });
+  }
 }
 
 addBookButton.addEventListener("click", (e) => {
@@ -125,6 +127,13 @@ function enableOverlay() {
 function disableOverlay() {
   overlayElement.style.display = "none";
 }
+
+// function updateBookValues() {
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     const currentDOMBook = document.querySelector("#book-" + i);
+//     myLibrary[i].isRead = currentDOMBook.checked;
+//   }
+// }
 
 overlayElement.addEventListener("dblclick", disableOverlay);
 closeFormButton.addEventListener("click", disableOverlay);
