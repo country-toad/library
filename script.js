ab42de9d4ca399d430eeb938f935eec5a7c0d65f
Raymond Zhong <rzhong669@gmail.com>
2023-05-06 17:55:12 -0500

    Refactor code to use prototype for toggleReadStatus

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-class Book {
-  constructor(
-    title = "Unknown",
-    author = "Unknown",
-    pages = 0,
-    isRead = false
-  ) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-    this.toggleReadStatus = function () {
-      this.isRead = !this.isRead;
-    };
-  }
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
+
+Book.prototype.toggleReadStatus = function () {
+  this.isRead = !this.isRead;
+};
 
 /*  Variables  */
 let myLibrary = [];
